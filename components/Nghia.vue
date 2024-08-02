d83561747e1b030f333ed2894e8b0d8746b71fdf
Horus031 <vominhnghia1878@gmail.com>
2024-08-02 18:21:46 +0700

    Update components 1

--- components/Nghia.vue
+++ components/Nghia.vue
@@ -1,25 +1,26 @@
 <template>
-  <v-container>
-    <v-row class="d-flex justify-space-between mt-16 align-center">
+  <v-container id="max-width-container">
+    <v-row class="ml-16 pl-6 d-flex flex-nowrap justify-space-around align-center mw-tc" height="600">
       <!-- Left Content -->
-      <v-col cols="12" md="7">
+      <v-col cols="12" md="7" width="50%" id="width-col" class="d-flex flex-column align-start" >
         <div class="text-subtitle-2 font-weight-bold yellow--text text--darken-4 mb-4">WORKFORCE OF TOMORROW</div>
-        <div class="text-h2 font-weight-bold line-height">Train, Work, Hire: All in</div>
-        <div class="text-h2 font-weight-bold font-italic text-decoration-underline yellow--text text--darken-4 pt-4">One Place.</div>
+        <div class="font-weight-bold line-height">Train, Work, Hire: All in <span class="line-height font-weight-bold font-italic text-decoration-underline yellow--text text--darken-4">One</span></div>
+        <div class="line-height font-weight-bold font-italic text-decoration-underline yellow--text text--darken-4">Place.</div>
         <div class="text-body-1 font-weight-medium mt-6">
           Train your community into a dynamic pool of skilled workers, ready to tackle one-off shifts seamlessly.
         </div>
-        <v-btn color="#f69351" class="pa-4 mt-6 text-no-wrap rounded-pill text-subtitle-2" style="width:150px; height:50px;">
-          Get Started
-        </v-btn>
+        <v-hover v-slot="{ hover }">
+          <v-btn :color= " hover ? '#dedfe3' : '#f69351' " :class="  hover ? 'black--text' : 'white--text' " class="mt-6 text-no-wrap rounded-pill text-subtitle-2 text-capitalize" style="width:150px; height:50px;">
+            Get Started
+          </v-btn>
+        </v-hover>
       </v-col>
       <!-- Right Image -->
-      <v-col cols="12" md="5">
+      <v-col cols="12" md="7" width="50%" id="max-width-img" class="d-inline-flex flex-column align-start ml-16 mt-16 pl-16 mr-n16">
         <v-img
-          aspect-ratio="16/9"
           src="https://cdn.prod.website-files.com/63877b2a41191ed2e132006b/6611a6969d96ffefe01fbb94_Component%201.png"
-          class="ml-md-16 pl-md-16"
-          max-width="100%"
+          class="ml-16 pl-16"
+      
         ></v-img>
       </v-col>
     </v-row>
@@ -32,8 +33,8 @@
           height="30"
           width="30"
         ></v-img>
-        <div class="text-h5 font-weight-bold mt-4">Organizations</div>
-        <div class="text-center mt-4" style="max-width: 550px;">
+        <div class="text-h5 font-weight-bold my-5">Organizations</div>
+        <div class="text-center mb-5" style="max-width: 550px;">
           Meet your staffing needs with a reliable pool of trained professionals, ready for on-demand, short-term roles.
         </div>
         <div class="text-h6 font-weight-bold mt-4 yellow--text text--darken-3">Learn more</div>
@@ -46,8 +47,8 @@
           height="30"
           width="30"
         ></v-img>
-        <div class="text-h5 font-weight-bold mt-4">Workers</div>
-        <div class="text-center mt-4" style="max-width: 550px;">
+        <div class="text-h5 font-weight-bold my-5">Workers</div>
+        <div class="text-center mb-5" style="max-width: 550px;">
           Build skills and find flexible jobs. Work wherever and whenever you want, on your terms.
         </div>
         <div class="text-h6 font-weight-bold mt-4 yellow--text text--darken-3">Learn more</div>
@@ -63,10 +64,27 @@ export default {
 </script>
 
 <style>
-.v-image__image--cover {
-  background-size: contain !important
-}
+
 .line-height {
-  line-height: 5px;
+  line-height: 90px !important;
+  font-size : 4.75rem !important;
+  letter-spacing: -0.0083333333em !important;
+  font-family: "Claritycity", sans-serif !important;
+  color: #00112b !important
+}
+#width-col {
+  max-width: 570px
+}
+#max-width-img {
+  max-width: 800px !important;
+  height: auto !important;
+  width: 100% !important
+}
+#max-width-container {
+  max-width: 100% !important;
+  max-height:100% !important
+}
+.mw-tc {
+  max-width: 100% !important
 }
 </style>
